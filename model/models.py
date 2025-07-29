1ec1ddc7d8b6b1376144a2be974fa555be84d423
Apoorva Shukla <apoorvashukla@Apoorvas-Mac-mini.local>
2025-07-29 09:24:14 +0530

    Updated the document analyzer

--- model/models.py
+++ model/models.py
@@ -1,2 +1,13 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import List, Union
+
+class MetaData(BaseModel):
+    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Title: str
+    Author: str
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]  # Can be "Not Available"
+    SentimentTone: str
