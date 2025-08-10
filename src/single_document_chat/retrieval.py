8bf1da1eb8e1674520ff530b34214f99f7594aab
Apoorva Shukla <apoorvashukla@Apoorvas-Mac-mini.local>
2025-08-10 18:07:25 +0530

    Complete the retriever pipeline for the multi doc chat

--- src/single_document_chat/retrieval.py
+++ src/single_document_chat/retrieval.py
@@ -1,6 +1,4 @@
 import sys
-
-# import streamlit as st
 from pathlib import Path
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
